c7503cd65276b2c362995769a8134ddbdcbd1d75
LucasH-uwin <hoogstrl@uwindsor.ca>
2026-03-24 18:06:36 -0400

    Add files via upload

---
+++ Java/Player.java
@@ -0,0 +1,13 @@
+package assignment;
+
+public class Player {
+	static public int playerCount;
+	public String name;
+	public int score = 0;
+	public String summary="";
+		
+	public Player() {
+			
+	}
+}
+
